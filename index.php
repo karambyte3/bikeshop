23047df23569d52c6b09dbcf922e74e62856426d
stefchuu <stefankalenderov@gmail.com>
2019-08-18 16:10:13 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+function route($routeUrls, $requestLogin = false) {
+    $pageFound = false;
+    $reqUrl = substr($_SERVER['REQUEST_URI'], 10);
+    $reqUrl = rtrim($reqUrl, 10);
+
+    if($reqUrl == '') {
+        home();
+        include_once  './pages/home.html';
+        return;
+    }
+
+    foreach($routeUrls as $url) {
+        if($reqUrl == $url) {
+
+            $funcName = str_replace('/', '_', $url);
+
+            $vars = call_user_func($funcName);
+            include_once  './pages/' . $url . '.html';
+
+            $pageFound = true;
+            return;
+        }
+    }
+
+    if($pageFound == false){
+        include './pages/error404.html';
+    }
+}
+
+function home() {
+    
+}
+
+route(
+    array(
+        'contact',
+        'error'
+        )
+);
+
+?>
+
+
